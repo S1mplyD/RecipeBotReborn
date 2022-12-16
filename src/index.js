4666bbc4965ef704e6c011aea9e2214e7328ef82
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-12-16 11:09:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
+
+mongoose.set("strictQuery", false);
 
 // connetti al db
 mongoose.connect(
@@ -17,8 +31,8 @@ mongoose.connect(
     if (err) throw err;
     console.log("Successfully connected mongoose");
     // INSERITO QUI PER AVVIARE IL TUTTO DOPO ESSERMI CONNESSO
-    // await registerEvents(client, "../events");
-    // await registerCommands(client, "../commands");
+    client.handleEvents();
+    client.handleCommands();
     await client.login(process.env.BOT_TOKEN);
   }
 );
