137c9cb1df2d86728586c1ca1a8cb7f536030b63
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-06-15 19:02:25 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+import { Events, CommandInteraction } from "discord.js";
+import { CustomClient } from "./client/client";
 import { config } from "dotenv";
-import { resolve } from "path";
-import { Client, GatewayIntentBits } from "discord.js";
-import "./client";
+import path, { resolve } from "path";
+import fs from "fs";
 
 config({ path: resolve(__dirname, "..", ".env") });
+
+// Create a new client instance
+const client = new CustomClient();
+client.setupInteractionHandler();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".ts"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.CLIENT_TOKEN);
