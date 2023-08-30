f847e39af7d7b4c9a29298e11fad474a947792e1
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-08-30 22:14:38 +0200

    add recipe schema

--- src/database/schema/recipe.ts
+++ src/database/schema/recipe.ts
@@ -1,58 +1,25 @@
-import { Schema, model, Model, Document } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
-interface MRecipeSchema {
+interface Recipe {
   url: string;
+  img: string;
   name: string;
   category: string;
   desc: string;
   lang: string;
-  img: string;
 }
 
-export interface RecipeDoc extends Document {
-  url: string;
-  name: string;
-  category: string;
-  desc: string;
-  lang: string;
-  img: string;
-}
+interface RecipeDocument extends Recipe, Document {}
 
-const RecipeSchema = new Schema({
-  url: {
-    type: Schema.Types.String,
-    require: true,
-    unique: true,
-  },
-  img: {
-    type: Schema.Types.String,
-  },
-  name: {
-    type: Schema.Types.String,
-    index: true,
-  },
-  category: {
-    type: Schema.Types.String,
-  },
-  desc: {
-    type: Schema.Types.String,
-  },
-  lang: {
-    type: Schema.Types.String,
-  },
+const recipeSchema = new Schema<RecipeDocument>({
+  url: { type: String, required: true },
+  img: { type: String, required: true },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  desc: { type: String, required: true },
+  lang: { type: String, required: true },
 });
 
-RecipeSchema.index({ name: "text" });
+const recipeModel = model<RecipeDocument>("recipeData", recipeSchema);
 
-// Added interface for building model
-interface guildInterface extends Model<RecipeDoc> {
-  build(attr: MRecipeSchema): RecipeDoc;
-}
-
-RecipeSchema.statics.build = (attr: MRecipeSchema) => {
-  return new Recipe(attr);
-};
-
-const Recipe = model<RecipeDoc, guildInterface>("Recipe", RecipeSchema);
-export { Recipe };
-export default Recipe;
+export default recipeModel;
