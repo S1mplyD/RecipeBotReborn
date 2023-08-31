2c1024b1f43c3bd0c125f8cc4342282445992514
Simone Ferretti <furrez04.2@gmail.com>
2023-08-31 19:14:08 +0200

    feat(countCommand): Set permission check
    Set adminId permission check for countCommand. Extended reply with "ephemeral: true" to show the answer privately.

--- src/commands/admin/countCommand.ts
+++ src/commands/admin/countCommand.ts
@@ -1,22 +1,28 @@
-import { SlashCommandBuilder } from "discord.js";
+import { BaseInteraction, Client, SlashCommandBuilder } from "discord.js";
 import { CommandInteraction } from "discord.js";
 import {
   getAllMembersCount,
   getServerNumber,
 } from "../../database/querys/guild";
+import adminIds from "constants"
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("count")
     .setDescription("Get the numbers of people using this bot"),
   async execute(interaction: CommandInteraction) {
-    const members: number | Error = await getAllMembersCount();
-    const server: number | Error = await getServerNumber();
-    if (members instanceof Error)
-      await interaction.reply("Some error occurred");
-    else
-      await interaction.reply({
-        content: `${members + ""} users in ${server + ""} servers`,
-      });
-  },
+    if (!adminIds[interaction.user.id]) {
+      const members: number | Error = await getAllMembersCount();
+      const server: number | Error = await getServerNumber();
+      if (members instanceof Error)
+        await interaction.reply("Some error occurred");
+      else
+        await interaction.reply({
+          content: `${members + ""} users in ${server + ""} servers`,
+          ephemeral: true,
+        });
+    } else {
+      await interaction.reply({content: "You cannot use this command", ephemeral: true});
+    }
+    }, 
 };
