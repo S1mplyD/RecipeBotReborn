5d4e882bc338fa0479e781a9bfbb82cbb895955e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-09-02 22:11:02 +0200

    fix getRandomRecipe

--- src/database/querys/recipe.ts
+++ src/database/querys/recipe.ts
@@ -13,13 +13,22 @@ export async function getCategories(lang: string) {
  * @param name research value
  * @returns RecipeType
  */
-export async function getRecipeName(name: string) {
+export async function getRecipeName(name: string, lang: string) {
   const recipes: RecipeType[] | null = await recipeModel.find({
     $or: [
       { name: { $regex: name, $options: "i" } },
       { desc: { $regex: name, $options: "i" } },
+      { category: { $regex: name, $options: "i" } },
     ],
+    lang: lang,
   });
   if (recipes.length < 1) return null;
   else return recipes[Math.floor(Math.random() * (recipes.length - 0 + 1)) + 0];
+}
+
+export async function getRandomRecipe(lang: string) {
+  const recipeNumbers = await recipeModel.find({ lang: lang }).countDocuments();
+  const random = Math.floor(Math.random() * recipeNumbers);
+  const recipe = await recipeModel.findOne({ lang: lang }).skip(random).lean();
+  return recipe;
 }
