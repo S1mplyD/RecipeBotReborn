1410731ab713395c87f044d276bd22dd70024bbe
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-09-04 17:57:23 +0200

    feat(list, recipe, timer): Add permission check
    Add permission check to listCommand, recipeCommand and timerCommand

--- src/commands/common/recipeCommand.ts
+++ src/commands/common/recipeCommand.ts
@@ -17,9 +17,11 @@ import {
   getUser,
 } from "../../database/querys/user";
 import { getUnpackedSettings } from "http2";
+import { checkPermissions } from "../../utils/checkPermissions";
 
 module.exports = {
   data: new SlashCommandBuilder()
+    .setDMPermission(false) // Command will not work in dm
     .setName("recipe")
     .setDescription("Show a Recipe")
     .addStringOption((option) =>
@@ -31,90 +33,80 @@ module.exports = {
 
   async execute(interaction: CommandInteraction, guild: GuildType) {
     const args = interaction.options.get("name");
+    const permissionError = checkPermissions(interaction);
 
-    if (!args) {
-      const recipe: RecipeType | null = await getRandomRecipe(guild.lang);
-      if (!recipe) interaction.reply("not found");
-      else {
-        const recipeEmbed = new EmbedBuilder()
-          .setTitle(recipe.name)
-          .setColor(constants.message.color)
-          .setDescription(recipe.desc);
-
-        const button = new ButtonBuilder()
-          .setCustomId("add")
-          .setLabel("Add")
-          .setStyle(ButtonStyle.Primary)
-          .setEmoji("⭐");
-
-        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
-
-        const response = await interaction.reply({
-          embeds: [recipeEmbed],
-          components: [row],
-        });
-        const collectorFilter = (i) => i.user.id === interaction.user.id;
-        try {
-          const confirmation = await response.awaitMessageComponent({
-            filter: collectorFilter,
-            time: 60000,
-          });
-          const user: UserType | Error = await getUser(interaction.user.id);
-          if (user instanceof Error) await createUser(interaction.user.id);
-          if (confirmation.customId === "add") {
-            await addRecipeToUserFavourite(interaction.user.id, recipe.url);
-            confirmation.update({
-              content: `Recipe ${recipe.name} added to favorites`,
+    if (!permissionError) {
+      if (!args) {
+        const recipe: RecipeType | null = await getRandomRecipe(guild.lang);
+        if (!recipe) interaction.reply("not found");
+        else {
+          const recipeEmbed = new EmbedBuilder()
+            .setTitle(recipe.name)
+            .setImage(recipe.img)
+            .setColor(constants.message.color)
+            .setDescription(recipe.desc)
+            .setURL(recipe.url)
+            .setTimestamp()
+            .setFooter({
+              text: "Category: " + recipe.category ?? " ",
+              iconURL: constants.botImage,
             });
-          } else {
-            interaction.reply("error occurred");
-          }
-        } catch (e) {
-          await interaction.reply({
-            content: "Confirmation not received within 1 minute, cancelling",
-            components: [],
+
+          // const button = new ButtonBuilder()
+          //   .setCustomId("add")
+          //   .setLabel("Add")
+          //   .setStyle(ButtonStyle.Primary)
+          //   .setEmoji("⭐");
+
+          // const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
+
+          const response = await interaction.reply({
+            embeds: [recipeEmbed],
+            // components: [row],
           });
+          // const collectorFilter = (i) => i.user.id === interaction.user.id;
+          // try {
+          //   const confirmation = await response.awaitMessageComponent({
+          //     filter: collectorFilter,
+          //     time: 60000,
+          //   });
+          //   const user: UserType | Error = await getUser(interaction.user.id);
+          //   if (user instanceof Error) await createUser(interaction.user.id);
+          //   if (confirmation.customId === "add") {
+          //     await addRecipeToUserFavourite(interaction.user.id, recipe.url);
+          //     confirmation.update({
+          //       content: `Recipe ${recipe.name} added to favorites`,
+          //     });
+          //   } else {
+          //     interaction.reply("error occurred");
+          //   }
+          // } catch (e) {
+          //   await interaction.reply({
+          //     content: "Confirmation not received within 1 minute, cancelling",
+          //     components: [],
+          //   });
+          // }
         }
+      } else {
+        let recipeName = args?.value as string;
+        console.log("Recipe Name:", recipeName);
+        const recipe = await getRecipeName(recipeName, guild.lang);
+        if (recipe) {
+          console.log("db name:", recipe.name);
+          const recipeEmbed = new EmbedBuilder()
+            .setTitle(recipe.name)
+            .setImage(recipe.img)
+            .setColor(constants.message.color)
+            .setDescription(recipe.desc)
+            .setURL(recipe.url)
+            .setTimestamp()
+            .setFooter({
+              text: "Category: " + recipe.category ?? " ",
+              iconURL: constants.botImage,
+            });
+          await interaction.reply({ embeds: [recipeEmbed] });
+        } else await interaction.reply("No matching recipe name found");
       }
-    } else {
-      let recipeName = args?.value as string;
-      console.log("Recipe Name:", recipeName);
-      const recipe = await getRecipeName(recipeName, guild.lang);
-      if (recipe) {
-        console.log("db name:", recipe.name);
-        const recipeEmbed = new EmbedBuilder()
-          .setTitle(recipe.name)
-          .setColor(constants.message.color)
-          .setFields([
-            {
-              name: "Regular field title",
-              value: "Some value here",
-            },
-            {
-              name: "\u200b",
-              value: "\u200b",
-              inline: false,
-            },
-            {
-              name: "Inline field title",
-              value: "Some value here",
-              inline: true,
-            },
-            {
-              name: "Inline field title",
-              value: "Some value here",
-              inline: true,
-            },
-            {
-              name: "Inline field title",
-              value: "Some value here",
-              inline: true,
-            },
-          ]);
-        //   .setImage(/*IMMAGINE RICETTA*/)
-        //   .setURL(/*LINK ALLA RICETTA*/);
-        await interaction.reply({ embeds: [recipeEmbed], ephemeral: true });
-      } else await interaction.reply("No matching recipe name found");
-    }
+    } else await interaction.reply({ content: permissionError, ephemeral: true });
   },
 };
--- src/commands/common/timerCommand.ts
+++ src/commands/common/timerCommand.ts
@@ -10,10 +10,12 @@ import {
   updateTimer,
 } from "../../database/querys/timers";
 import { client } from "../..";
+import { checkPermissions } from "../../utils/checkPermissions";
 
 const hourMultiplier = 1000 * 60 * 60;
 module.exports = {
   data: new SlashCommandBuilder()
+    .setDMPermission(false) // Command will not work in dm
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels) // Requires the "ManageChannels" permission to see the command (eg: Mods)
     .setName("timer")
     .setDescription("Send a random timed recipe")
@@ -39,129 +41,135 @@ module.exports = {
     // ------------------------------------------------------------------------------
 
     const args = interaction.options.get("time");
+    const permissionError = checkPermissions(interaction);
 
-    // Check if command has arguments
-    if (!args) {
-      const timer = await getTimerByGuildId(interaction.guildId!);
+    if (!permissionError) {
+      // Check if command has arguments
+      if (!args) {
+        const timer = await getTimerByGuildId(interaction.guildId!);
 
-      // If the guild already has a timer, reply with its time. (1)
-      if (timer) {
-        const reply =
-          timer.time / hourMultiplier == 1
-            ? `Current timer is set to ${timer.time / hourMultiplier} hour`
-            : `Current timer is set to ${timer.time / hourMultiplier} hours`;
-        interaction.reply({ content: reply, ephemeral: true });
-      }
-      // (1) Otherwise, prompt to add a timer
-      else
-        interaction.reply({
-          content:
-            "No timer set. please add a time amount (in hours) after the `/timer` command",
-          ephemeral: true,
-        });
-    } else {
-      // Command has no arguments
-      const timer = await getTimerByGuildId(interaction.guildId!);
-
-      if (typeof args.value === "string") {
-        const lowerCaseArgs = args.value.toLowerCase();
-
-        if (lowerCaseArgs === "off") {
-          // ##########################
-          // ###   SET TIMER OFF    ###
-          // ##########################
-
-          // And check if guild has a timer
-          if (timer) {
-            await stopTimer(timer);
-            interaction.reply({ content: "Timer stopped", ephemeral: true });
-          } else interaction.reply({ content: "No timer found", ephemeral: true }); // Guild has no timer
-        } else if (lowerCaseArgs === "on") {
-          // ##########################
-          // ###    SET TIMER ON    ###
-          // ##########################
-
-          // And check if guild has a timer
-          if (timer) {
-            await setTimerStatus(timer, true);
-            await startTimer(timer, client, true);
-            interaction.reply({ content: "Timer started", ephemeral: true });
-          } else interaction.reply({ content: "No timer found", ephemeral: true }); // Guild has no timer
+        // If the guild already has a timer, reply with its time. (1)
+        if (timer) {
+          const reply =
+            timer.time / hourMultiplier == 1
+              ? `Current timer is set to ${timer.time / hourMultiplier} hour`
+              : `Current timer is set to ${timer.time / hourMultiplier} hours`;
+          interaction.reply({ content: reply, ephemeral: true });
         }
-        // All other cases where timer is neither "off" nor "on" -> "args" is a number
-        else {
-          if (!timer) {
+        // (1) Otherwise, prompt to add a timer
+        else
+          interaction.reply({
+            content:
+              "No timer set. please add a time amount (in hours) after the `/timer` command",
+            ephemeral: true,
+          });
+      } else {
+        // Command has no arguments
+        const timer = await getTimerByGuildId(interaction.guildId!);
+
+        if (typeof args.value === "string") {
+          const lowerCaseArgs = args.value.toLowerCase();
+
+          if (lowerCaseArgs === "off") {
             // ##########################
-            // ###  CREATE NEW TIMER  ###
+            // ###   SET TIMER OFF    ###
             // ##########################
 
-            try {
-              const newTimer = await createTimer(
-                interaction.guildId!,
-                interaction.channelId,
-                args.value as unknown as number,
-                guild.lang
-              );
+            // And check if guild has a timer
+            if (timer) {
+              await stopTimer(timer);
+              interaction.reply({ content: "Timer stopped", ephemeral: true });
+            } else
+              interaction.reply({ content: "No timer found", ephemeral: true }); // Guild has no timer
+          } else if (lowerCaseArgs === "on") {
+            // ##########################
+            // ###    SET TIMER ON    ###
+            // ##########################
 
-              if (typeof newTimer == "string")
-                // Creation ERROR (input time was less than 1 or more than 24)
-                interaction.reply({ content: newTimer, ephemeral: true });
-              else {
-                // Creation SUCCESSFUL (input time was more than 1 and less than 24)
-                await startTimer(newTimer, client, true);
+            // And check if guild has a timer
+            if (timer) {
+              await setTimerStatus(timer, true);
+              await startTimer(timer, client, true);
+              interaction.reply({ content: "Timer started", ephemeral: true });
+            } else
+              interaction.reply({ content: "No timer found", ephemeral: true }); // Guild has no timer
+          }
+          // All other cases where timer is neither "off" nor "on" -> "args" is a number
+          else {
+            if (!timer) {
+              // ##########################
+              // ###  CREATE NEW TIMER  ###
+              // ##########################
+
+              try {
+                const newTimer = await createTimer(
+                  interaction.guildId!,
+                  interaction.channelId,
+                  args.value as unknown as number,
+                  guild.lang
+                );
+
+                if (typeof newTimer == "string")
+                  // Creation ERROR (input time was less than 1 or more than 24)
+                  interaction.reply({ content: newTimer, ephemeral: true });
+                else {
+                  // Creation SUCCESSFUL (input time was more than 1 and less than 24)
+                  await startTimer(newTimer, client, true);
+                  interaction.reply({
+                    content: "timer started",
+                    ephemeral: true,
+                  });
+                }
+              } catch {
                 interaction.reply({
-                  content: "timer started",
+                  content: `Value **${args.value}** is not a valid timer argument`,
                   ephemeral: true,
                 });
               }
-            } catch {
-              interaction.reply({
-                content: `Value **${args.value}** is not a valid timer argument`,
-                ephemeral: true,
-              });
-            }
-          } else {
-            // ######################
-            // ###  UPDATE TIMER  ###
-            // ######################
+            } else {
+              // ######################
+              // ###  UPDATE TIMER  ###
+              // ######################
 
-            try {
-              const updatedTimer = await updateTimer(
-                timer,
-                args.value as unknown as number
-              );
-              if (updatedTimer)
-                // Creation ERROR (input time was less than 1 or more than 24)
-                interaction.reply({ content: updatedTimer, ephemeral: true });
-              else {
-                // Creation SUCCESSFUL (input time was more than 1 and less than 24)
-                const newTimer: TimerType | null = await getTimerByGuildId(
-                  timer.guildId
+              try {
+                const updatedTimer = await updateTimer(
+                  timer,
+                  args.value as unknown as number
                 );
+                if (updatedTimer)
+                  // Creation ERROR (input time was less than 1 or more than 24)
+                  interaction.reply({ content: updatedTimer, ephemeral: true });
+                else {
+                  // Creation SUCCESSFUL (input time was more than 1 and less than 24)
+                  const newTimer: TimerType | null = await getTimerByGuildId(
+                    timer.guildId
+                  );
 
-                // Start the updated timer
-                await startTimer(newTimer!, client, true);
-                if (timer) {
-                  const reply =
-                    args.value == "1"
-                      ? `Timer set to ${args.value} hour`
-                      : `Timer set to ${args.value} hours`;
-                  interaction.reply({
-                    content: reply,
-                    ephemeral: true,
-                    /*components: [row]*/  //Buttons integration WIP
-                  }); 
+                  // Start the updated timer
+                  await startTimer(newTimer!, client, true);
+                  if (timer) {
+                    const reply =
+                      args.value == "1"
+                        ? `Timer set to ${args.value} hour`
+                        : `Timer set to ${args.value} hours`;
+                    interaction.reply({
+                      content: reply,
+                      ephemeral: true,
+                      /*components: [row]*/ //Buttons integration WIP
+                    });
+                  }
                 }
+              } catch {
+                interaction.reply({
+                  content: `value "${args.value}" is not a valid timer argument`,
+                  ephemeral: true,
+                });
               }
-            } catch {
-              interaction.reply({
-                content: `value "${args.value}" is not a valid timer argument`,
-                ephemeral: true
-              });
             }
           }
         }
       }
-    }
+    } else
+      await interaction.reply({ content: permissionError, ephemeral: true });
   },
 };
