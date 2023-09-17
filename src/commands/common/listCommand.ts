a46668054fa60e55fa12f8d09e5c9eacfd62dc73
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-09-17 21:17:36 +0200

    feat(listCommand): use cleaned list

--- src/commands/common/listCommand.ts
+++ src/commands/common/listCommand.ts
@@ -5,26 +5,37 @@ import constants from "../../utils/constants";
 import loadLanguage from "../../utils/loadLanguage";
 import { getCategories } from "../../database/querys/recipe";
 import { checkPermissions } from "../../utils/checkPermissions";
+import { cleaned } from "../../utils/listCleaner";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setDMPermission(false) // Command will not work in dm
     .setName("list")
-    .setDescription("Show the list of categories"),
+    .setDescription("Show the list of categories")
+    .addStringOption((option) =>
+      option
+        .setName("type")
+        .setDescription("Country | Cuisine")
+        .setRequired(false)
+    ),
   async execute(interaction: CommandInteraction, guild: GuildType) {
     const permissionError = checkPermissions(interaction);
+    const args = interaction.options.get("type");
 
     if (!permissionError) {
       const languagePack = loadLanguage(guild.lang);
-      const categories = await getCategories(guild.lang);
+
+      // const categories = await getCategories(guild.lang);
+      const categories: string[] | undefined = await cleaned(guild.lang);
       let str = "";
-      if (categories.length < 1) str = "No categories found";
-      else {
-        for (let i = 0; i < categories.length; i++) {
-          str += `${i + 1}) ${categories[i]}\n`;
+      if (categories) {
+        if (categories.length < 1) str = "No categories found";
+        else {
+          for (let i = 0; i < categories.length; i++) {
+            str += `${i + 1}) ${categories[i]}\n`;
+          }
         }
       }
-
       const list = new EmbedBuilder()
         .setTitle(languagePack.code.categoryList.title)
         .setColor(constants.message.color)
