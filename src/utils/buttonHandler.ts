7a412487ae20209a4a972430084d210c779e91c0
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-10-07 14:25:09 +0200

    feat(buttonHandler): Add list commandhandler, fix bug
    Moved "if(!recipe)" condition inside favorite button handlers to not interfer with other buttons. Add handler for "/list" command buttons, this is called whenever the buttons are pressed but will respond only if issues are encountered (hence the 2500 timeout).

--- src/utils/buttonHandler.ts
+++ src/utils/buttonHandler.ts
@@ -7,29 +7,25 @@ import {
   removeRecipeFromFavorite,
 } from "../database/querys/user";
 import { getRecipeById } from "../database/querys/recipe";
+import { setTimeout } from "timers/promises";
 
 export async function handleButtonInteraction(
-  interaction: ButtonInteraction,
+  interaction: ButtonInteraction
   // guild: GuildType
 ) {
   const buttonId = interaction.customId.split(":");
-  console.log(
-    "buttonId: ",
-    buttonId[0],
-    "\nbuttonValue: ",
-    buttonId[1]
-  );
+  console.log("buttonId: ", buttonId[0], "\nbuttonValue: ", buttonId[1]);
+
   const recipe: RecipeType | null = await getRecipeById(buttonId[1]);
-
-  if (!recipe) {
-    await interaction.reply("Recipe not found.");
-    return;
-  }
-
   const user: UserType | Error = await getUser(interaction.user.id);
+
   if (user instanceof Error) await createUser(interaction.user.id);
 
   if (buttonId[0] === "add_favorite_recipe") {
+    if (!recipe) {
+      await interaction.reply("Recipe not found.");
+      return;
+    }
     const addRecipe = await addRecipeToUserFavorite(
       interaction.user.id,
       recipe.url
@@ -43,6 +39,10 @@ export async function handleButtonInteraction(
       });
     }
   } else if (buttonId[0] === "remove_favorite_recipe") {
+    if (!recipe) {
+      await interaction.reply("Recipe not found.");
+      return;
+    }
     try {
       const removeRecipe = await removeRecipeFromFavorite(
         interaction.user.id,
@@ -62,10 +62,30 @@ export async function handleButtonInteraction(
         ephemeral: true,
       });
     }
+  } else if (
+    buttonId[0] === "category_list_forward" ||
+    buttonId[0] === "category_list_backwards"
+  ) {
+    await setTimeout(2500);
+    try {
+      await interaction.reply({
+        content: "Interaction has expired, use **/list** command again",
+        ephemeral: true,
+      });
+      await setTimeout(10000);
+      interaction.deleteReply();
+    } catch {
+      console.log(`${buttonId[0]} was pressed`);
+    }
   } else {
-    await interaction.reply({
-      content: "Unknown button clicked.",
-      ephemeral: true,
-    });
+    await setTimeout(2500);
+    try {
+      await interaction.reply({
+        content: "Unknown button clicked.",
+        ephemeral: true,
+      });
+    } catch {
+      console.log("interaction already replied");
+    }
   }
 }
