a9a7399b1939e664c3d4623ad3ce8cf71728c9a4
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-10-10 23:43:37 +0200

    feat(dateTime): Add Date function
    Add a currentDateTime() function to get a timestamp of when an action is performed.

---
+++ src/utils/dateTime.ts
@@ -0,0 +1,15 @@
+export default function currentDateTime() {
+  try {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  } catch {
+    return "";
+  }
+}
