fd1c60d59f8a04cedb7bceab7ed72cc776cd5698
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-10-23 22:57:01 +0200

    feat!(user): Update user query
    Update user db query to utilize the new schema; As of now the "Date" field gets the value with "new Date()", this will be replaced with the date system utilized in 5a0aa03
    
    BREAKING CHANGE: user query changed to include date field

--- src/database/querys/user.ts
+++ src/database/querys/user.ts
@@ -3,8 +3,16 @@ import recipeModel from "../schema/recipe.model";
 import userModel from "../schema/user.model";
 
 export async function createUser(userId: string) {
-  const user: UserType | null = await userModel.create({ userId: userId });
-  if (!user) return new Error("cannot create user");
+  try {
+    const user = await userModel.create({
+      userId: userId,
+      favoriteRecipes: [] 
+    });
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 export async function getUser(userId: string) {
@@ -28,7 +36,7 @@ export async function isRecipeInUserFavorite(userId: string, url: string) {
   if (!user) {
     return new Error("User not found");
   }
-  if (user.favoriteRecipes.includes(url)) {
+  if (user.favoriteRecipes.some((recipe) => recipe.url === url)) {
     return true;
   } else {
     return false;
@@ -36,19 +44,38 @@ export async function isRecipeInUserFavorite(userId: string, url: string) {
 }
 
 export async function addRecipeToUserFavorite(userId: string, url: string) {
+
   if (await isRecipeInUserFavorite(userId, url)) {
     return new Error("Recipe already added to favorites");
   }
 
-  const recipe = await userModel.updateOne(
-    { userId: userId },
-    { $push: { favoriteRecipes: url } }
-  );
+  try {
 
-  if (recipe.modifiedCount < 1) {
-    return new Error("Cannot add recipe to favorites");
+    const currentDate = new Date();
+
+    const updateResult = await userModel.updateOne(
+      { userId: userId },
+      {
+        $push: {
+          favoriteRecipes: {
+            url: url,
+            date: currentDate,
+          },
+        },
+      }
+    );
+
+    if (updateResult.modifiedCount < 1) {
+      return new Error("Cannot add recipe to favorites");
+    }
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return new Error("An error occurred while adding the recipe to favorites");
   }
 }
+
 
 export async function removeRecipeFromFavorite(userId: string, url: string) {
   if (!(await isRecipeInUserFavorite(userId, url))) {
