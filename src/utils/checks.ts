63fd7d12d7b8c9a44f4f2d5a904e305174717cef
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-11-04 12:33:23 +0100

    chore(checkPermissions): Rename file and add function
    Add checkVoteAndAnswer() function. Rename file from "checkPermissions" to "check" since it is now used to check both bot's permissions and votes.

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -28,3 +28,22 @@ export function checkPermissions(interaction: CommandInteraction) {
   }
   return null;
 }
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function checkVoteAndAnswer(user) {
+  const voted = false; // TODO: REMOVE BOOL
+  // await votesClient.hasVoted(interaction.user.id);
+
+  if (voted) {
+    console.log("User has voted!");
+    return true;
+  } else {
+    console.log("User has not voted!");
+
+    const content =
+      "Looks like you haven't voted the bot yet\n[Click here](https://top.gg/bot/657369551121678346/vote) to go to the voting page";
+
+    return content;
+  }
+}
