01c88f08a1c2174fb110aacfc7f6943beeff9fa6
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-12-01 14:06:04 +0100

    feat!(bot): Integrate stats
    Add stat update when bot gets invited and kicked. stats document ID will be stored in guild document to link them in one-way (Can get stats info from guild but not guild info from stats); This is done to improve stats privacy.
    
    From commit 010cda3 but now is in bot.ts instead of index.ts

--- src/bot.ts
+++ src/bot.ts
@@ -6,11 +6,15 @@ import path, { resolve } from "path";
 import fs from "fs";
 import mongoose from "mongoose";
 import { createGuild, getGuildByGuildId } from "./database/querys/guild";
-import { GuildType } from "./utils/types";
+import { GuildType, StatsType } from "./utils/types";
 import guildModel from "./database/schema/guild.model";
 import { startAllTimer, stopTimer } from "./utils/timers";
 import timerModel from "./database/schema/timers.model";
 import { getTimerByGuildId } from "./database/querys/timers";
+import {
+  createGuildStats,
+  updateRemoveGuildStat,
+} from "./database/querys/stats";
 
 config({ path: resolve(__dirname, "..", ".env") });
 
@@ -48,18 +52,42 @@ client.once(Events.ClientReady, async (c) => {
   gilde.forEach(async (guild) => {
     const guilddb: GuildType | Error = await getGuildByGuildId(guild.id);
     if (guilddb instanceof Error) {
-      await createGuild(guild.id, guild.name, guild.memberCount);
+      const addDate = new Date;
+      let statsId = "";
+      try {
+        const newGuildStat: Error | StatsType = await createGuildStats(addDate);
+        console.log(newGuildStat);
+        if (!(newGuildStat instanceof Error)) {
+          statsId = newGuildStat._id;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      await createGuild(guild.id, guild.name, guild.memberCount, statsId);
     }
   });
   await startAllTimer(client);
 });
 
 client.on("guildCreate", async (guild) => {
+  console.log(`bot added to new guild (${guild.id})`);
+  const addDate = new Date;
+  let statsId = "";
+  try {
+    const newGuildStat: Error | StatsType = await createGuildStats(addDate);
+    console.log(newGuildStat);
+    if (!(newGuildStat instanceof Error)) {
+      statsId = newGuildStat._id;
+    }
+  } catch (error) {
+    console.log(error);
+  }
   try {
     const newGuild: Error | GuildType = await createGuild(
       guild.id,
       guild.name,
-      guild.memberCount
+      guild.memberCount,
+      statsId
     );
 
     console.log(newGuild);
@@ -78,6 +106,18 @@ client.on("guildDelete", async (guild) => {
     );
   } catch (error) {
     console.error("Error when trying to delete timer from database:", error);
+  }
+
+  const removeDate = new Date;
+  try {
+    const guildDocument: Error | GuildType = await getGuildByGuildId(guild.id);
+    if (!(guildDocument instanceof Error)) {
+      await updateRemoveGuildStat(guildDocument.statsId, removeDate);
+    }
+  } catch {
+    console.error(
+      `Error when updating removeDate field for guild with ID: ${guild.id}`
+    );
   }
 
   try {
